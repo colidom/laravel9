90abed2e53f7e4ac655dfd4da083a696551244bf
colidom <colidom@outlook.com>
2023-04-07 23:31:40 +0100

    Plantillas de Blade - Componentes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('title', 'Home')
 @section('meta-description', 'Home meta-description')
 
-@section('content')
-<h1>Home</h1>
-@endsection
+@section('content') 
+@endsection--}}
+<x-layouts.app title="Home" meta-description="Home meta Description" :sum="2+2">
+    <h1>Home</h1>
+</x-layouts.app>
