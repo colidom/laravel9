00b828c5d7e280790d553bc1216f40e641e208cd
colidom <colidom@outlook.com>
2023-04-09 22:45:00 +0100

    Introducción a Eloquent el ORM de Laravel 9

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 
 class PostController
 {
     public function index()
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::get(); // Asume que posts será la tabla ya que el modelo se llama Post en singular
 
         return view('blog', ['posts' => $posts]);
     }
